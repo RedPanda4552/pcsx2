bfa0a98b3419faf8740f8de35a1f127da15267ec
RedPanda4552 <redpanda4552@gmail.com>
2025-02-23 12:46:30 -0500

    Add capacity for PS1

--- pcsx2/SIO/Memcard/MemcardPS1.cpp
+++ pcsx2/SIO/Memcard/MemcardPS1.cpp
@@ -2,6 +2,7 @@
 #include "PrecompiledHeader.h"
 
 #include "SIO/Memcard/MemcardPS1.h"
+#include "SIO/Memcard/MemcardHostBase.h"
 
 MemcardPS1::MemcardPS1(u32 unifiedSlot)
 	: MemcardBase(unifiedSlot)
@@ -13,6 +14,17 @@ MemcardPS1::~MemcardPS1() = default;
 Memcard::Type MemcardPS1::GetType()
 {
 	return Memcard::Type::PS1;
+}
+
+bool MemcardPS1::ValidateCapacity()
+{
+	if (this->GetMemcardHost() != nullptr)
+	{
+		const u32 fileSize = this->GetMemcardHost()->GetSize();
+		return fileSize == MemcardPS1::CARD_SIZE_BYTES;
+	}
+
+	return false;
 }
 
 void MemcardPS1::ExecuteCommand()
