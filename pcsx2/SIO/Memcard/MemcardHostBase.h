77d74b696d390911641d3c85bf6a243e3796bc81
RedPanda4552 <redpanda4552@gmail.com>
2025-10-09 17:01:31 -0400

    Checkpoint

--- pcsx2/SIO/Memcard/MemcardHostBase.h
+++ pcsx2/SIO/Memcard/MemcardHostBase.h
@@ -1,6 +1,11 @@
 
 #pragma once
 
+// Length of the string found at the front of a memory card superblock, indicating it has been formatted.
+// Excludes the version number.
+constexpr u8 FORMAT_STRING_LENGTH = 28;
+constexpr const char* FORMAT_STRING = "Sony PS2 Memory Card Format ";
+
 class MemcardHostBase
 {
 private:
@@ -14,8 +19,12 @@ public:
 	virtual ~MemcardHostBase();
 	
 	std::string GetPath();
+	bool IsFormatted();
 	
 	virtual bool IsOpened() = 0;
+	// The max theoretical capacity of the PS2 filesystem is 2 GB; this must be at least a u32 to hold that size,
+	// but we are also using negatives to signal when a host type has no physical size associated with it. This
+	// is typed as s64 to give us the depth needed for that.
 	virtual s64 GetSize() = 0;
 
 	virtual void Write(u32 addr, std::vector<u8>& src) = 0;
